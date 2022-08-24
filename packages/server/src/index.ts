8dde93f0bafecf749e77e638e72dfb9504853383
James <jamesyj.hong@gmail.com>
2022-08-23 23:15:11 -0400

    init project

---
+++ packages/server/src/index.ts
@@ -0,0 +1,31 @@
+import express, { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+import authMiddleware from "./middleware/authMiddleware";
+
+dotenv.config();
+
+const app = express();
+const authHeaderRouter = express.Router();
+const port = process.env.PORT;
+
+authHeaderRouter.use(authMiddleware);
+
+app.get("/", (req: Request, res: Response) => {
+  res.redirect("/health");
+});
+
+authHeaderRouter.get(
+  "/health",
+  (req: Request, res: Response, next: NextFunction) => {
+    res.json({
+      status: "UP",
+      message: `Running on http://localhost:${port}`,
+    });
+  },
+);
+
+app.use(authHeaderRouter);
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
