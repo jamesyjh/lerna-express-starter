184a3c7ad1d34cb19cee6ec1926936ad3eeb077e
James <jamesyj.hong@gmail.com>
2022-08-27 12:10:50 -0400

    feat: add openapi doc template

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,5 +1,8 @@
 import express, { NextFunction, Request, Response } from "express";
-import dotenv from "dotenv";
+import * as dotenv from "dotenv";
+import * as path from "path";
+import * as YAML from "yamljs";
+import * as swaggerUi from "swagger-ui-express";
 import authMiddleware from "./middleware/authMiddleware";
 import { logger, morganMiddleware } from "@lerna-express-starter/logger";
 
@@ -10,8 +13,12 @@ const authHeaderRouter = express.Router();
 const port = process.env.PORT || 2000;
 const VERSION = "/v1";
 
+const apiDocumentation = YAML.load(path.join(__dirname, "./openapi.yaml"));
+
 app.use(morganMiddleware);
 authHeaderRouter.use(authMiddleware);
+
+app.use(`${VERSION}/api-docs`, swaggerUi.serve, swaggerUi.setup(apiDocumentation));
 
 app.get("/", (req: Request, res: Response) => {
   res.redirect(`${VERSION}/health`);
