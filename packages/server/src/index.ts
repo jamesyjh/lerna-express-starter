dac5aa11f17a3ea86efc70ee4aaaaade62aa6f06
James <jamesyj.hong@gmail.com>
2022-08-27 11:42:27 -0400

    feat: implement custom logger

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,31 +1,31 @@
 import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import authMiddleware from "./middleware/authMiddleware";
+import { logger, morganMiddleware } from "@lerna-express-starter/logger";
 
 dotenv.config();
 
 const app = express();
 const authHeaderRouter = express.Router();
 const port = process.env.PORT || 2000;
+const VERSION = "/v1";
 
+app.use(morganMiddleware);
 authHeaderRouter.use(authMiddleware);
 
 app.get("/", (req: Request, res: Response) => {
-  res.redirect("/health");
+  res.redirect(`${VERSION}/health`);
 });
 
-authHeaderRouter.get(
-  "/health",
-  (req: Request, res: Response, next: NextFunction) => {
-    res.json({
-      status: "UP",
-      message: `Running on http://localhost:${port}`,
-    });
-  },
-);
+authHeaderRouter.get(`${VERSION}/health`, (req: Request, res: Response, next: NextFunction) => {
+  res.json({
+    status: "UP",
+    message: `Running on http://localhost:${port}`,
+  });
+});
 
 app.use(authHeaderRouter);
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  logger.debug(`⚡️Server is running at http://localhost:${port}`);
 });
